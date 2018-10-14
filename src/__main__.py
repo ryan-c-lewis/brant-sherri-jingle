0a85e25d390574a03cab04b383d4eaa697307659
ryan-c-lewis <ryan.cp.lewis@gmail.com>
2018-10-13 23:25:18 -0600

    Create trimmed file once we've found the jingle

--- src/__main__.py
+++ src/__main__.py
@@ -1,15 +1,26 @@
 from audio_offset_finder import audio_offset_finder
+from pydub import AudioSegment
+import ntpath
+import os
 
+JINGLE_MASTER_FILEPATH = "dep/master-jingle-opening.mp3"
+JINGLE_LENGTH = 6.5
 
-def get_offset_seconds(filepath):
+def find_jingle_and_trim(filepath):
     print("Processing file: " + filepath)
+    new_filepath = "out/" + ntpath.basename(filepath)[:-4] + "_trimmed.mp3"
     offset, score = audio_offset_finder.find_offset(
         filepath,
-        "dep/master-jingle-opening.mp3",
+        JINGLE_MASTER_FILEPATH,
         correl_nframes=50)
-    print("RESULT: " + str(offset) + " seconds (confidence=" + str(score) + ")")
-    return offset
+    print("Found jingle at " + str(offset) + " seconds (confidence=" + str(score) + ")")
+    if not os.path.exists("out"):
+        os.makedirs("out")
+    full_podcast = AudioSegment.from_mp3(filepath)
+    trimmed_podcast = full_podcast[offset*1000:((offset+JINGLE_LENGTH)*1000)]
+    trimmed_podcast.export(new_filepath, format="mp3")
+    print("Saved trim file to: " + new_filepath)
 
 
 if __name__ == '__main__':
-    offset = get_offset_seconds("dep/09_27_18_podcast2.mp3")
+    find_jingle_and_trim("dep/09_27_18_podcast2.mp3")
